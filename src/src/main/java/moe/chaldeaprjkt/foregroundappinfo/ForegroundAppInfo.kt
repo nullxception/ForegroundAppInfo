30f7bd2c9d78bca264d21d281671b1eb17194854
Nauval Rizky <enuma.alrizky@gmail.com>
2020-07-17 20:25:12 +0700

    Add listener functionality with ScheduledThreadPoolExecutor

--- src/src/main/java/moe/chaldeaprjkt/foregroundappinfo/ForegroundAppInfo.kt
+++ src/src/main/java/moe/chaldeaprjkt/foregroundappinfo/ForegroundAppInfo.kt
@@ -3,10 +3,17 @@ package moe.chaldeaprjkt.foregroundappinfo
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import moe.chaldeaprjkt.foregroundappinfo.reader.ActivityReader
 import moe.chaldeaprjkt.foregroundappinfo.reader.UsageStatsReader
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 class ForegroundAppInfo(private val context: Context) {
+    var timeout = 1000L
+
+    private lateinit var callback: (ApplicationInfo) -> Unit
     private val reader by lazy {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
             UsageStatsReader(context)
@@ -15,7 +22,38 @@ class ForegroundAppInfo(private val context: Context) {
         }
     }
 
+    private val pool by lazy {
+        ScheduledThreadPoolExecutor(1)
+    }
+
+    private val handler by lazy {
+        Handler(Looper.getMainLooper())
+    }
+
+    private fun runCallback() {
+        if (!::callback.isInitialized)
+            return
+
+        handler.post { callback.invoke(read()) }
+        pool.schedule(Runnable(::runCallback), timeout, TimeUnit.MILLISECONDS)
+    }
+
     fun read(): ApplicationInfo =
         context.packageManager.getPackageInfo(reader.read(), 0).applicationInfo
 
+    fun startListening(callback: (ApplicationInfo) -> Unit) {
+        callback.let {
+            handler.post { it.invoke(read()) }
+            this.callback = it
+        }
+
+        pool.schedule(Runnable(::runCallback), timeout, TimeUnit.MILLISECONDS)
+    }
+
+    fun stopListening() {
+        if (pool.isTerminated || !::callback.isInitialized)
+            return
+
+        pool.shutdownNow()
+    }
 }
